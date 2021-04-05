cbc0fb6fa1fd24294b0c1c15041f39a15d230b6d
ryzecous <r3j9c89@gmail.com>
2021-04-05 15:30:16 -0400

    api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router
+const mockPortfolioController = require('../controllers/mockPortfolioController')
+const portfolioController = require('../controllers/portfolioController')
+const userController = require('../controllers/userController')
+
+router.route('/api/mockportfolio')
+    .get(mockPortfolioController.getMockPortfolio)
+    .post(mockPortfolioController.createMockPortfolio)
+    .put(mockPortfolioController.updateMockPortfolio)
+    .delete(mockPortfolioController.deleteMockPortfolio)
+;
+
+router.route('/api/portfolio')
+    .get(portfolioController.getPortfolio)
+    .post(portfolioController.createPortfolio)
+    .put(portfolioController.updatePortfolio)
+    .delete(portfolioController.deletePortfolio)
+;
+
+router.route('/api/users/:id')
+    .get(userController.getUser)
+    .post(userController.createUser)
+    .put(userController.updateUser)
+    .delete(userController.deleteUser)
+;
