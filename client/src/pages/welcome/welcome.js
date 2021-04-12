47c07a68fd3c0fdb8dd5615f7800081bc1e801c3
Matthew Hayden <matthayden09@gmail.com>
2021-04-12 14:45:43 -0500

    added crypto logos to animations

--- client/src/pages/welcome/welcome.js
+++ client/src/pages/welcome/welcome.js
@@ -16,16 +16,53 @@ class Welcome extends React.Component {
                     </form>
                 </div>
                 <ul className="bg-bubbles">
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
-                    <li className="square"></li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=010" alt="Bitcoin"></img>
+
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=010" alt="Ethereum"></img>
+
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/tether-usdt-logo.svg?v=010" alt="Tether"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/cardano-ada-logo.svg?v=010" alt="Cardano"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/polkadot-new-dot-logo.svg?v=010" alt="Polkadot"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/cosmos-atom-logo.svg?v=010" alt="Cosmos"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/binance-coin-bnb-logo.svg?v=010" alt="Binance Coin"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/xrp-xrp-logo.svg?v=010" alt="XRP"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/uniswap-uni-logo.svg?v=010" alt="Uniswap"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/dogecoin-doge-logo.svg?v=010" alt="Dogecoin"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/litecoin-ltc-logo.svg?v=010" alt="Litecoin"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/chainlink-link-logo.svg?v=010" alt="Chainlink"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/stellar-xlm-logo.svg?v=010" alt="Stellar"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/vechain-vet-logo.svg?v=010" alt="VeChain"></img>
+                    </li>
+                    <li className="logo">
+                        <img src="https://cryptologos.cc/logos/sushiswap-sushi-logo.svg?v=010" alt="SushiSwap"></img>
+                    </li>
                 </ul>
 
             </div>
