3d6bbf3886f025eba3fd5c3d8a6fb1f124ed51cb
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-04-15 14:56:23 -0500

    render api data to prices page via a table

--- client/src/pages/prices/prices.js
+++ client/src/pages/prices/prices.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import API from './../../utils/API.js';
-
+import Table from './../../components/table'
 const Investments = () => {
     const id = useRef(0);
     const currentId = useRef(0);
@@ -21,12 +21,62 @@ const Investments = () => {
             mounted = false;
         }
     }, [])
+    const columns = () => {
+        return [
+            {
+                header: 'Name',
+                field: 'name',
+            },
+            {
+                header: 'Price',
+                field: 'price',
+            },
+            {
+                header: 'Change',
+                field: 'change',
+            },
+            {
+                header: 'Volume',
+                field: 'volume',
+            },
+            {
+                header: 'Market cap',
+                field: 'marketCap',
+                // content: (row) => {
+                    // This returns the current row object, 
+                    // Allowing you to override default value of a field 
+                    // You need to return a Html element within content
+                    // return <h1 style={{ background: 'red'}}>{row.marketCap}</h1>
+                // }
+            },
+            {
+                header: 'Supply',
+                field: 'supply',
+                // style: {
+                    // this styles will be add the the current column cell
+                    // color: 'lightgreen'
+                // }
+            }
+        ]
+    }
+
+    const rows = () => {
+        return prices.map((item) => ({
+            name: item.name,
+            price: item.price,
+            change: item['1d'].price_change,
+            volume: item['1d'].volume,
+            marketCap: item['1d'].market_cap_change,
+            supply: item.max_supply,
+        }))
+    }
     return (
         <>
             <div>PRICES</div>
-            <ul>  
-            {prices.map((item, index) => <il key={index}>{item.name}</il>)} 
-            </ul>
+            <Table
+                columns={columns()}
+                rows={rows()}
+            />
         </>
     )
 };
