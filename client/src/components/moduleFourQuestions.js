0c41f3c85d9ac72496382982fabd9c5bd00598ca
ryzecous <r3j9c89@gmail.com>
2021-04-17 12:38:49 -0400

    updated module 4 questions structure

--- client/src/components/moduleFourQuestions.js
+++ client/src/components/moduleFourQuestions.js
@@ -4,56 +4,62 @@ const ModuleFourQuestions = () => {
         questions : [
             {
                 questionText: "How many keys does a typical crypto wallet use in a transaction?",
-                answerOptions: [
-                    { answerText: 'As many as the user chooses upon creation', isCorrect: false },
-                    { answerText: '1 key', isCorrect: false },
-                    { answerText: '2 keys', isCorrect: true },
-                    { answerText: '1 key per coin being traded', isCorrect: false },
+                correctAnswer: "2 keys",
+                answers: [
+                    'As many as the user chooses upon creation',
+                    '1 key',
+                    '2 keys',
+                    '1 key per coin being traded',
                 ],
             },
             {
                 questionText: "What are crypto keys used for?",
-                answerOptions: [
-                    { answerText: 'Unclocking a cryptocoin', isCorrect: false },
-                    { answerText: 'digitally signing data', isCorrect: true },
-                    { answerText: 'viewing the ledger of a cryptocurrency', isCorrect: false },
-                    { answerText: 'unlocking the device your currency is stored on', isCorrect: false },
+                correctAnswer: "digitally signing data",
+                answers: [
+                    'Unclocking a cryptocoin',
+                    'digitally signing data',
+                    'viewing the ledger of a cryptocurrency',
+                    'unlocking the device your currency is stored on'
                 ],
             },
             {
                 questionText: "When investing in cryptocurrency and holding for a long time it is most secure to use:",
-                answerOptions: [
-                    { answerText: 'a \'Hot\' wallet', isCorrect: false },
-                    { answerText: 'a piggy bank', isCorrect: false },
-                    { answerText: 'a \'Cold\' wallet', isCorrect: true },
-                    { answerText: 'A web application', isCorrect: false },
+                correctAnswer: "a \'Cold\' wallet",
+                answers: [
+                    'a \'Hot\' wallet',
+                    'a piggy bank',
+                    'a \'Cold\' wallet',
+                    'A web application'
                 ],
             },
             {
                 questionText: "Which of the following is not a type of cryptocyrrency wallet",
-                answerOptions: [
-                    { answerText: 'Software wallet', isCorrect: false },
-                    { answerText: 'Hardware wallet', isCorrect: false },
-                    { answerText: 'Paper wallet', isCorrect: false },
-                    { answerText: 'Open wallet', isCorrect: true },
+                correctAnswer: "Open wallet",
+                answers: [
+                    'Software wallet',
+                    'Hardware wallet',
+                    'Paper wallet',
+                    'Open wallet'
                 ],
             },
             {
                 questionText: "If a wallet is connected to the internet it is considered:",
-                answerOptions: [
-                    { answerText: 'a \'Hot\' wallet', isCorrect: true },
-                    { answerText: 'a \'Cold\' wallet', isCorrect: false },
-                    { answerText: 'backed-up', isCorrect: false },
-                    { answerText: 'insured', isCorrect: false },
+                correctAnswer: "a \'Hot\' wallet",
+                answers: [
+                    'a \'Hot\' wallet',
+                    'a \'Cold\' wallet', 
+                    'backed-up',
+                    'insured'
                 ],
             },
             {
                 questionText: "Which type of wallet is considered to be \"out of date\" and \"unreliable\"",
-                answerOptions: [
-                    { answerText: '\'Hot\' wallets', isCorrect: false },
-                    { answerText: 'paper wallets', isCorrect: true },
-                    { answerText: 'hardware wallets', isCorrect: false },
-                    { answerText: 'duct-tape wallets', isCorrect: false },
+                correctAnswer: "paper wallets",
+                answers: [
+                    '\'Hot\' wallets',
+                    'paper wallets',
+                    'hardware wallets',
+                    'duct-tape wallets'
                 ],
             },
         ]
