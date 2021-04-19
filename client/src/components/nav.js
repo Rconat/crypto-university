760e63c0af097fcce3e5e3d7efebfbcce610ae95
Matthew Hayden <matthayden09@gmail.com>
2021-04-19 09:00:21 -0500

    initial branch commit

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -6,7 +6,7 @@ const Nav = () => (
         <div className="container-fluid">
             <Link to="/about" className="navbar-brand" href="#">Crypto University</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
