4a9b4fc1e9c36332dfffefbd23e6c94acce66fb7
Matthew Hayden <matthayden09@gmail.com>
2021-04-22 08:57:12 -0500

    finished general layout

---
+++ client/src/components/dashboardModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function dashboardModal() {
+    return (
+        <div>
+            <p className="dashboard-modal-text">
+                This is your central hub for Crypto University. Below you will find modules that will guide you on your path to learning everything there is to know about crypto currency! Each module contains a quiz at the end that will test you on what you learned in that module. Once you complete all of the modules, then you will earn your diploma and will be equiped with all the information you need to begin buying, selling and trading crypto. Good luck!
+                </p>
+        </div>
+    )
+}
