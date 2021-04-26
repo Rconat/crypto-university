84307cb165037523d3a397d9afe4bc4a08b2ac9a
ryzecous <r3j9c89@gmail.com>
2021-04-26 15:48:03 -0400

    more styling

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -23,7 +23,7 @@ const Nav = () => {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
                 <Link to="/about" className="navbar-brand
                 cu-about hvr-grow" href="#">
